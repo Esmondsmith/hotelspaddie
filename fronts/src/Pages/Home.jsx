c266d038b2fed972054bcbff3e2b06d3ab596341
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2025-06-29 17:19:51 +0100

    Update Home page layout and code formatting
    - Reorder components: BestHotels now appears before Hero
    - Update import statements to use double quotes for consistency
    - Improve code formatting and semicolon usage
    - Update Footer component formatting
    - Maintain clean component structure with Footer as last element

--- fronts/src/Pages/Home.jsx
+++ fronts/src/Pages/Home.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import Hero from '../Components/Hero/Hero'
-import BestHotels from '../Components/BestHotels/BestHotels'
+import React from "react";
+import Hero from "../Components/Hero/Hero";
+import BestHotels from "../Components/BestHotels/BestHotels";
 // import Offers from '../Components/Offers/Offers'
 // import NewCollections from '../Components/NewCollections/NewCollections'
 // import Newsletter from '../Components/Newsletter/Newsletter'
-import Banner from '../Components/Banner/Banner'
-import Footer from '../Components/Footer/Footer'
-import Navbar from '../Components/Navbar/Navbar'
-
+import Banner from "../Components/Banner/Banner";
+import Footer from "../Components/Footer/Footer";
+import Navbar from "../Components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
-      <Hero />
       <BestHotels />
-      {/* <Offers />  
-      <NewCollections />
-      <Newsletter /> */}
+      <Hero />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
